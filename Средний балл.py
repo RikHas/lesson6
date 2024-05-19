5c7e2bb048ef47b3ee217053c9ff97e5b113b552
RikHas <skp47@mail.ru>
2024-05-19 16:22:52 +0300

    Update Средний балл.py

--- Средний балл.py
+++ Средний балл.py
@@ -1,10 +1,11 @@
-grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-n_grades = (sum(grades[0])/len(grades[0]),
-            sum(grades[1])/len(grades[1]),
-            sum(grades[2])/len(grades[2]),
-            sum(grades[3])/len(grades[3]),
-            sum(grades[4])/len(grades[4]))
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-sort_s = sorted(students)
-new_s = dict(zip(sort_s,n_grades))
-print(new_s)
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+average_grades = []
+
+for i in grades:
+    grade_average = sum(i[:]) / len(i[:])
+    average_grades.append(grade_average)
+
+sort_students = sorted(students)
+dictionary = dict(zip(sort_students, average_grades))
+print(dictionary)
